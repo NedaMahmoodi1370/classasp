8cd94a78be4801c10dc1fdc600a683fae00bd325
NedaMahmoodi1370 <mahmodirad@chargoon.com>
2022-05-22 22:52:02 +0430

    s02 assignment

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    //    1    
+    //   121   
+    //  12321  
+    // 1234321 
+    //123454321
+
+
+    int row = 7;
+
+    for(int index = 0; index < 7; index++)
+    {
+        row--;
+        for(int sapce = 0; sapce < row; sapce++)
+        {
+            System.Console.Write(" ");
+        }
+        for( int counter = 0; counter < index; counter++)
+        {
+            System.Console.Write(counter+1);
+        }
+        for( int counter = index + 1; counter > 0; counter--)
+        {
+            System.Console.Write(counter);
+        }
+        for(int sapce = 0; sapce < row; sapce++)
+        {
+            System.Console.Write(" ");
+        }
+        System.Console.WriteLine();
+    }   
